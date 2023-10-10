2cfc2cc0933c479a8c0d028067d7ca344f0eed1d
DanieleIn <141851778+leadcash@users.noreply.github.com>
2023-10-10 21:57:24 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -240,6 +240,26 @@ const currentDate = new Date();
 const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
 const currentMonth = months[currentDate.getMonth()];
 const currentDay = currentDate.getDate();
+
+ <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2355712514627799');
+  fbq('track', 'PageView');
+  fbq('track', 'ViewContent');
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2355712514627799&ev=PageView&noscript=1"/>
+  <img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2355712514627799&ev=ViewContent&noscript=1"/>
+</noscript>
 const currentYear = currentDate.getFullYear();
 const formattedDate = currentDayOfWeek + ', ' + ('0' + (currentDate.getMonth() + 1)).slice(-2) + '/' + ('0' + currentDay).slice(-2) + '/' + currentYear;
 document.getElementById('currentDate').textContent = formattedDate;
